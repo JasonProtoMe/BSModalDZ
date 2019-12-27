7ae31d05076d5dee6edd7ca8be57727662a5e09e
Jason <Jason@proto-me.com>
2019-12-27 16:10:14 -0500

    first commit

---
+++ ModalDZ/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ModaldzConfig(AppConfig):
+    name = 'ModalDZ'
---
+++ ModalDZ/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+
+
+import os
+
+# Create your views here.
+
+def index(request):
+    context = {}
+    if request.method == 'POST':
+        savefiles(request)
+        return HttpResponse(context)
+
+    return render(request, 'ModalDZ/index.html', context)
+
+def savefiles(request):
+    # A way to access Django-files format
+    uploaded_files = [request.FILES.get('file[%d]' % i)
+                     for i in range(0, len(request.FILES))]
+
+    # saving onto the media folder
+    file_path = os.path.join(os.getcwd(), 'media/')
+
+    for file in uploaded_files:
+        default_storage.save(file_path, ContentFile(file.read()))
